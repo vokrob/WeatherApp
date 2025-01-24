959447814730739fe26f702c8e6759b8a599c233
vokrob <106639488+vokrob@users.noreply.github.com>
2025-01-24 23:40:50 +0700

    Customize project

---
+++ app/src/main/java/com/vokrob/weatherapp/MainActivity.kt
@@ -0,0 +1,102 @@
+package com.vokrob.weatherapp
+
+import android.graphics.Color.parseColor
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.vokrob.weatherapp.ui.theme.WeatherAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+
+        setContent {
+            WeatherAppTheme { }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun WeatherScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.horizontalGradient(
+                    colors = listOf(
+                        Color(parseColor("#59469d")),
+                        Color(parseColor("#643d67"))
+                    )
+                )
+            )
+    )
+    {
+        Column(modifier = Modifier.fillMaxSize()) {
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                item {
+                    Text(
+                        text = "Mostly Cloudy",
+                        fontSize = 20.sp,
+                        color = Color.White,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(top = 48.dp),
+                        textAlign = TextAlign.Center
+                    )
+
+                    Image(
+                        painter = painterResource(id = R.drawable.cloudy_sunny),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(150.dp)
+                            .padding(top = 8.dp)
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
